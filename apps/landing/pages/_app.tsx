0142f11c1d385f996a2a6ad77628471afb92de42
Miquel de Domingo <miquelddg@gmail.com>
2021-12-11 00:14:25 +0100

    chore: updated .eslintrc config

--- apps/landing/pages/_app.tsx
+++ apps/landing/pages/_app.tsx
@@ -1,18 +1,17 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
 import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to landing!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
-}
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+const CustomApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Welcome to landing!</title>
+    </Head>
+    <main className="app">
+      <Component {...pageProps} />
+    </main>
+  </>
+);
 
 export default CustomApp;
