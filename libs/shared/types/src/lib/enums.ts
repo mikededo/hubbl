5b5d3d060c14fd4994d7ac3db15d8f0cca077841
Miquel de Domingo <miquelddg@gmail.com>
2021-12-12 15:25:17 +0100

    init(libs): added `shared/types` lib

---
+++ libs/shared/types/src/lib/enums.ts
@@ -0,0 +1,14 @@
+enum AppTheme {
+  DARK = 'dark',
+  LIGHT = 'light'
+}
+
+enum ThemeColor {
+  GRAY = '#4A5568',
+  RED = '#F56565',
+  ORANGE = '#FBCF33',
+  GREEN = '#68D391',
+  BLUE = '#2196F3'
+}
+
+export { AppTheme, ThemeColor };
