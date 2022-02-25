be4c2bb4195405ee01a13cc3be28b583d3872c89
Miquel de Domingo <miquelddg@gmail.com>
2022-02-25 14:26:17 +0100

    chore(utils): fix jest coverage config HBL-146

--- libs/utils/jest.config.js
+++ libs/utils/jest.config.js
@@ -11,5 +11,6 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/utils'
+  coverageDirectory: '../../coverage/libs/utils',
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts']
 };
