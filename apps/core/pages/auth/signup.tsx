432c6f2363eb38c5da5b05031d5b2b68db2cec16
Miquel de Domingo <miquelddg@gmail.com>
2022-02-25 14:33:33 +0100

    feat(core): added signup page logic Components still to be tested HBL-146 #time 12h

---
+++ apps/core/pages/auth/signup.tsx
@@ -0,0 +1,117 @@
+import React, { useCallback, useState } from 'react';
+
+import { AnimatePresence, motion } from 'framer-motion';
+
+import { Divider, Stack, Typography } from '@mui/material';
+
+import { AuthLayout, Pages } from '../../components';
+
+const { FormWrapper, FooterLink, FormFooter, SideImage, StepOne, StepTwo } =
+  Pages.Signup;
+
+type SignupFormFields = {
+  user: Pages.Signup.StepOneFields;
+  gym: Pages.Signup.StepTwoFields;
+};
+
+const InitialFormState: SignupFormFields = {
+  user: { firstName: '', lastName: '', email: '', password: '' },
+  gym: { name: '', email: '', phone: '' }
+};
+
+const SignUp = () => {
+  const [formState, setFormState] = useState(InitialFormState);
+
+  const [step, setStep] = useState<number>(0);
+  const [stepOneRef, setStepOneRef] = useState(null);
+  const [stepTwoRef, setStepTwoRef] = useState(null);
+
+  const getContainerHeight = useCallback(() => {
+    if ((!stepOneRef && step === 0) || (!stepTwoRef && step === 1)) {
+      return 0;
+    }
+
+    return (step ? stepTwoRef : stepOneRef).getBoundingClientRect().height;
+  }, [step, stepOneRef, stepTwoRef]);
+
+  const handleOnSubmitStepOne = (data: Pages.Signup.StepOneFields) => {
+    setFormState((prev) => ({ ...prev, user: data }));
+    setStep(1);
+  };
+
+  const handleOnBlurStepTwo = (data: Pages.Signup.StepTwoFields) => {
+    setFormState((prev) => ({ ...prev, gym: data }));
+  };
+
+  const handleOnGoBack = () => {
+    setStep(0);
+  };
+
+  const handleOnSubmit = (data: Pages.Signup.StepTwoFields) => {
+    console.log(data);
+  };
+
+  return (
+    <AuthLayout>
+      <FormWrapper>
+        <Stack direction="column" gap={{ xs: 2, sm: 4 }}>
+          <Stack gap={1.5}>
+            <Typography variant="h2">Sign up</Typography>
+            <Typography variant="subtitle1">
+              Register now and start managing your gym like a boss!
+            </Typography>
+          </Stack>
+
+          <Stack
+            gap={1.5}
+            component={motion.main}
+            animate={{ height: getContainerHeight() }}
+          >
+            <AnimatePresence>
+              {step === 0 && (
+                <motion.section
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  ref={(ref) => setStepOneRef(ref)}
+                >
+                  <StepOne
+                    initialFormState={formState.user}
+                    onSubmit={handleOnSubmitStepOne}
+                  />
+                </motion.section>
+              )}
+            </AnimatePresence>
+
+            <AnimatePresence>
+              {step === 1 && (
+                <motion.section
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  ref={(ref) => setStepTwoRef(ref)}
+                >
+                  <StepTwo
+                    initialFormState={formState.gym}
+                    onBack={handleOnGoBack}
+                    onBlur={handleOnBlurStepTwo}
+                    onSubmit={handleOnSubmit}
+                  />
+                </motion.section>
+              )}
+            </AnimatePresence>
+          </Stack>
+
+          <Divider />
+
+          <FormFooter variant="subtitle2">
+            Already with an account?{' '}
+            <FooterLink href="/auth/login">Log in now!</FooterLink>
+          </FormFooter>
+        </Stack>
+      </FormWrapper>
+
+      <SideImage />
+    </AuthLayout>
+  );
+};
+
+export default SignUp;
