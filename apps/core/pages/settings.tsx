a2b2615f5de9a3db50ca004a4134f25bc5e059cf
Miquel de Domingo <miquelddg@gmail.com>
2022-03-06 00:07:50 +0100

    feat(core): put user changes HBL-170

--- apps/core/pages/settings.tsx
+++ apps/core/pages/settings.tsx
@@ -1,7 +1,11 @@
+import { RequiredUserInfoFields } from '@hubbl/ui/components';
+
+import { useAppContext } from '@hubbl/data-access/contexts';
 import {
   ContentCard,
   PageHeader,
   SettingsLogout,
+  SettingsUserInfo,
   SideNav
 } from '@hubbl/ui/components';
 import { Box, Stack } from '@mui/material';
@@ -30,48 +34,73 @@ const entries = [
   }
 ];
 
-const Settings = () => (
-  <Stack
-    direction="row"
-    justifyContent="stretch"
-    gap={4}
-    sx={{ height: '100vh', width: '100wh', overflow: 'hidden' }}
-  >
-    <SideNav entries={entries} header="Gym name" selected="settings" />
+const Settings = () => {
+  const { user, API } = useAppContext();
 
-    <Box
-      sx={{
-        overflow: 'auto',
-        width: '100%',
-        height: '100%',
-        padding: '48px 32px 32px'
-      }}
+  const handleOnSaveInfo = (data: RequiredUserInfoFields) => {
+    console.log(data)
+    API.user.update(data);
+  };
+
+  const mapUserToValues = (): RequiredUserInfoFields => {
+    if (!user) {
+      return undefined;
+    }
+
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      phone: user.phone,
+      gender: user.gender
+    };
+  };
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="stretch"
+      gap={4}
+      sx={{ height: '100vh', width: '100wh', overflow: 'hidden' }}
     >
-      <Stack
-        direction="column"
-        spacing={3}
+      <SideNav entries={entries} header="Gym name" selected="settings" />
+
+      <Box
         sx={{
-          height: '100%',
+          overflow: 'auto',
           width: '100%',
-          maxWidth: '1120px'
+          height: '100%',
+          padding: '48px 32px 32px'
         }}
       >
-        <PageHeader
-          title="Settings"
-          breadcrumbs={[
-            { href: '/', label: 'Settings' },
-            { href: '/', label: 'Settings' }
-          ]}
-        />
+        <Stack
+          direction="column"
+          spacing={3}
+          sx={{
+            height: '100%',
+            width: '100%',
+            maxWidth: '1120px'
+          }}
+        >
+          <PageHeader
+            title="Settings"
+            breadcrumbs={[{ href: '/', label: 'Settings' }]}
+          />
 
-        <SettingsLogout header="User full name" subtitle="Gym owner" />
+          <SettingsLogout header="User full name" subtitle="Gym owner" />
 
-        <ContentCard sx={{ minHeight: '364px' }} />
-        <ContentCard sx={{ minHeight: '206px' }} />
-        <ContentCard sx={{ minHeight: '281px' }} />
-      </Stack>
-    </Box>
-  </Stack>
-);
+          <SettingsUserInfo
+            defaultValues={mapUserToValues()}
+            onSubmit={handleOnSaveInfo}
+          />
+          {/* TODO: TEst all the form components */}
+
+          <ContentCard sx={{ minHeight: '206px' }} />
+          <ContentCard sx={{ minHeight: '281px' }} />
+        </Stack>
+      </Box>
+    </Stack>
+  );
+};
 
 export default Settings;
