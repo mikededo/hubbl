3cf96ea1f43cf6b1a9d164a01f2bdbbeb6dd6c37
Miquel de Domingo <miquelddg@gmail.com>
2022-03-06 00:27:35 +0100

    feat(ui): memoized `SettingsUserInfo` component Memoizing the component, the amount of renders of the form is reduced and it also removes visual effects issues. HBL-170 #time 15m

--- libs/ui/components/src/lib/SettingsUserInfo/SettingsUserInfo.tsx
+++ libs/ui/components/src/lib/SettingsUserInfo/SettingsUserInfo.tsx
@@ -1,13 +1,14 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { Gender as GenderEnum, SingleHandler } from '@hubbl/shared/types';
 import { Save } from '@mui/icons-material';
-import { Button, Stack, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 
 import ContentCard from '../ContentCard';
+import LoadingButton from '../LoadingButton';
 import {
   Email,
   EmailConfirmation,
@@ -27,76 +28,93 @@ const Content = styled(ContentCard)(({ theme }) => ({
   gap: theme.spacing(3)
 }));
 
-const SaveButton = styled(Button)({ alignSelf: 'flex-end' });
+const SaveButton = styled(LoadingButton)({ alignSelf: 'flex-end' });
 
 type SettingsUserInfoProps = {
   /**
    * Optional initial fields of the form. Otherwise, empty strings are
    * used
+   *
+   * @default undefined
    */
   defaultValues?: RequiredUserInfoFields;
 
   /**
+   * Whether the input is loading or not
+   *
+   * @default false
+   */
+  loading?: boolean;
+
+  /**
    * Callback to run on when the form has been submitted. Returns
    * the values of the form.
+   *
+   * @default undefined
    */
   onSubmit?: SingleHandler<RequiredUserInfoFields>;
 };
 
-const SettingsUserInfo = ({
-  defaultValues,
-  onSubmit
-}: SettingsUserInfoProps) => {
-  const methods = useForm<SettingsUserInfoFields>({
-    defaultValues: { gender: GenderEnum.OTHER, ...defaultValues }
-  });
+const SettingsUserInfo = React.memo<SettingsUserInfoProps>(
+  ({ defaultValues, loading = false, onSubmit }) => {
+    const methods = useForm<SettingsUserInfoFields>({
+      defaultValues: { gender: GenderEnum.OTHER, ...defaultValues },
+      shouldFocusError: false,
+      shouldUnregister: false
+    });
 
-  /**
-   * Calls onSubmit, if any, only with the required fields
-   */
-  const handleOnSubmit = (data: SettingsUserInfoFields) => {
-    const { emailConfirmation, ...required } = data;
+    /**
+     * Calls onSubmit, if any, only with the required fields
+     */
+    const handleOnSubmit = (data: SettingsUserInfoFields) => {
+      const { emailConfirmation, ...required } = data;
+      onSubmit?.(required);
+    };
 
-    onSubmit?.(required);
-  };
+    useEffect(() => {
+      if (defaultValues) {
+        methods.reset({
+          ...defaultValues,
+          emailConfirmation: defaultValues.email
+        });
+      }
+    }, [defaultValues, methods]);
 
-  useEffect(() => {
-    if (defaultValues) {
-      methods.reset({
-        ...defaultValues,
-        emailConfirmation: defaultValues.email
-      });
-    }
-  }, [defaultValues, methods]);
+    return (
+      <Content as="form" onSubmit={methods.handleSubmit(handleOnSubmit)}>
+        <Typography variant="h6">Basic information</Typography>
 
-  return (
-    <Content as="form" onSubmit={methods.handleSubmit(handleOnSubmit)}>
-      <Typography variant="h6">Basic information</Typography>
+        <FormProvider {...methods}>
+          <Stack gap={2} width="100%">
+            <Stack gap={3} direction="row">
+              <FirstName />
+              <LastName />
+            </Stack>
 
-      <FormProvider {...methods}>
-        <Stack gap={2} width="100%">
-          <Stack gap={3} direction="row">
-            <FirstName />
-            <LastName />
+            <Stack gap={3} direction="row" width="100%">
+              <Email />
+              <EmailConfirmation />
+            </Stack>
+
+            <Stack gap={3} direction="row" width="100%">
+              <Phone />
+              <Gender />
+            </Stack>
           </Stack>
+        </FormProvider>
 
-          <Stack gap={3} direction="row" width="100%">
-            <Email />
-            <EmailConfirmation />
-          </Stack>
-
-          <Stack gap={3} direction="row" width="100%">
-            <Phone />
-            <Gender />
-          </Stack>
-        </Stack>
-      </FormProvider>
-
-      <SaveButton startIcon={<Save />} type="submit">
-        Save
-      </SaveButton>
-    </Content>
-  );
-};
+        <SaveButton
+          label="Save"
+          loading={loading}
+          type="submit"
+          startIcon={<Save />}
+        />
+      </Content>
+    );
+  },
+  (prev, next) =>
+    JSON.stringify(prev.defaultValues) === JSON.stringify(next.defaultValues) &&
+    prev.loading === next.loading
+);
 
 export default SettingsUserInfo;
