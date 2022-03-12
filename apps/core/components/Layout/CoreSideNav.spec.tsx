babb116c4784de0046505871479663a14b98c23d
Miquel de Domingo <miquelddg@gmail.com>
2022-03-12 10:14:54 +0100

    feat(core): added `CoreSideNav` component HBL-157 #time 5m

---
+++ apps/core/components/Layout/CoreSideNav.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import CoreSideNav from './CoreSideNav';
+
+describe('<CoreSideNav />', () => {
+  it('should render properly', () => {
+    // The component is not furtherly tested, as it does not
+    // add more complexity other than what the SideNav component
+    // does, which has already the required tests
+    const { container } = render(
+      <CoreSideNav header="Test header" selected="dashboard" />
+    );
+
+    expect(container).toBeInTheDocument();
+  });
+});
---
+++ apps/core/components/Layout/CoreSideNav.tsx
@@ -0,0 +1,51 @@
+import { SideNav } from '@hubbl/ui/components';
+
+const entries = [
+  {
+    name: 'GENERAL',
+    hidden: true,
+    entries: {
+      dashboard: { label: 'Dashboard', href: '/dashboard' },
+      virtualGyms: { label: 'Virtual gyms', href: '#' },
+      events: { label: 'Events', href: '#' }
+    }
+  },
+  {
+    name: 'PERSONAL',
+    entries: {
+      trainers: { label: 'Trainers', href: '#' },
+      workers: { label: 'Workers', href: '#' },
+      clients: { label: 'Clients', href: '#' }
+    }
+  },
+  {
+    name: 'SETTINGS',
+    entries: { settings: { label: 'Settings', href: '/settings' } }
+  }
+];
+
+type CoreSideNavOptions =
+  | 'dashboard'
+  | 'virtualGyms'
+  | 'events'
+  | 'trainers'
+  | 'workers'
+  | 'clients'
+  | 'settings';
+
+type CoreSideNavProps = {
+  /**
+   * Header of the core side navigation
+   */
+  header: string;
+
+  /**
+   * Selected value of the navigation bar
+   */
+  selected: CoreSideNavOptions;
+};
+const CoreSideNav = ({ header, selected }: CoreSideNavProps) => (
+  <SideNav entries={entries} header={header} selected={selected} />
+);
+
+export default CoreSideNav;
