05003fbc9a63ffb8e77b7a854808de3b8790bc50
Miquel de Domingo <miquelddg@gmail.com>
2022-03-14 16:23:58 +0100

    feat(utils): function to normalize 24h time HBL-158

--- libs/utils/src/lib/functions.spec.ts
+++ libs/utils/src/lib/functions.spec.ts
@@ -1,4 +1,4 @@
-import { notForwardAny, notForwardOne } from './functions';
+import { notForwardAny, notForwardOne, timeNormalizer } from './functions';
 
 describe('functions', () => {
   describe('notForwardOne', () => {
@@ -22,6 +22,38 @@ describe('functions', () => {
 
     it('should return false if the prop exists in the array', () => {
       expect(notForwardAny(['given', 'prop'])('prop')).toBeFalsy();
+    });
+  });
+
+  describe('timeNormalizer', () => {
+    it('should normalize the valid values', () => {
+      const values = [
+        { value: '', valid: '' },
+        { value: '1', valid: '1' },
+        { value: '12', valid: '12' },
+        { value: '12:3', valid: '12:3' },
+        { value: '12:34', valid: '12:34' },
+        { value: '03:45', valid: '03:45' },
+        { value: '23:55', valid: '23:55' }
+      ];
+
+      values.forEach(({ value, valid }) => {
+        expect(timeNormalizer(value)).toBe(valid);
+      });
+    });
+
+    it('should normalize the invalid values', () => {
+      const values = [
+        { value: '9', valid: '' },
+        { value: '24', valid: '2' },
+        { value: '12:6', valid: '12' },
+        { value: '35:6', valid: '' },
+        { value: '99:99', valid: '' },
+      ];
+
+      values.forEach(({ value, valid }) => {
+        expect(timeNormalizer(value)).toBe(valid);
+      });
     });
   });
 });
--- libs/utils/src/lib/functions.ts
+++ libs/utils/src/lib/functions.ts
@@ -15,3 +15,36 @@ export const notForwardOne = (given: PropertyKey) => (prop: PropertyKey) =>
  */
 export const notForwardAny = (given: PropertyKey[]) => (prop: PropertyKey) =>
   !given.includes(prop);
+
+/**
+ * Normalizes/Masks the given string value to a 24h time format
+ *
+ * @param value The value to normalize/mask
+ */
+export const timeNormalizer = (value: string): string => {
+  if (!value.length) {
+    return '';
+  }
+
+  const splitted = value.split(':').join('');
+
+  switch (splitted.substring(0, 4).length) {
+    case 1:
+      return /[0-2]/.test(splitted) ? value : '';
+    case 2:
+      return /([01][0-9]|2[0-3])/.test(splitted)
+        ? value
+        : timeNormalizer(value.substring(0, 1));
+    case 3:
+      return /([01][0-9]|2[0-3])[0-5]/.test(splitted)
+        ? `${value.substring(0, 2)}:${value.charAt(value.length - 1)}`
+        : timeNormalizer(value.substring(0, 2));
+    case 4:
+    default:
+      return /([01][0-9]|2[0-3])[0-5][0-9]/.test(splitted)
+        ? value.substring(0, 5)
+        : timeNormalizer(
+            `${value.substring(0, 2)}:${value.charAt(value.length - 1)}`
+          );
+  }
+};
