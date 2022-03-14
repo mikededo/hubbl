612659155ab41c17ddb68459ea59aefac6526af5
Miquel de Domingo <miquelddg@gmail.com>
2022-03-14 17:16:29 +0100

    feat(ui): added `TimeInput` component HBL-158

--- libs/ui/components/src/lib/TimeInput/TimeInput.tsx
+++ libs/ui/components/src/lib/TimeInput/TimeInput.tsx
@@ -1,31 +1,65 @@
 import { UseFormRegisterReturn } from 'react-hook-form';
-import InputMask from 'react-input-mask';
 
 import { AccessTime } from '@mui/icons-material';
-import { InputAdornment } from '@mui/material';
+import {
+  InputAdornment,
+  InputBaseProps,
+  Stack,
+  Typography,
+  TypographyVariants
+} from '@mui/material';
 
-import Input, { InputProps } from '../Input';
+import InputBase from '../InputBase';
+import { timeNormalizer } from '@hubbl/utils';
+import React from 'react';
 
 export type TimeInputProps = {
-  registerResult: UseFormRegisterReturn;
-} & InputProps;
+  /**
+   * Text to display as the label of the input
+   */
+  label: string;
+
+  /**
+   * Variant of the label component
+   * @default 'h6'
+   */
+  labelVariant?: keyof Pick<TypographyVariants, 'h6' | 'body1'>;
+
+  /**
+   * Result value of registering the input into the
+   * `useForm`'s hook
+   */
+  registerResult?: UseFormRegisterReturn;
+} & InputBaseProps;
 
 const TimeInput = ({
+  label,
+  labelVariant = 'h6',
   registerResult,
   ...props
-}: TimeInputProps): JSX.Element => (
-  <InputMask mask="99:99" maskChar={null} {...registerResult}>
-    {() => (
-      <Input
+}: TimeInputProps): JSX.Element => {
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.target.value = timeNormalizer(e.target.value);
+  };
+
+  return (
+    <Stack direction="column" gap={1} width="100%">
+      <Typography variant={labelVariant} component="label">
+        {label}
+      </Typography>
+
+      <InputBase
         {...props}
+        {...registerResult}
+        onChange={handleOnChange}
         startAdornment={
           <InputAdornment position="start">
             <AccessTime />
           </InputAdornment>
         }
       />
-    )}
-  </InputMask>
-);
+    </Stack>
+  );
+};
 
 export default TimeInput;
