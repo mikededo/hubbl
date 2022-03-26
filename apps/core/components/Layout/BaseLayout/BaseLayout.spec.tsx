7bc02b57faafacb1d3d644c9da9975f68788693b
Miquel de Domingo <miquelddg@gmail.com>
2022-03-26 16:27:11 +0100

    test(core): improve coverage HBL-179 #time 5m

--- apps/core/components/Layout/BaseLayout/BaseLayout.spec.tsx
+++ apps/core/components/Layout/BaseLayout/BaseLayout.spec.tsx
@@ -1,9 +1,24 @@
-import { createTheme, ThemeProvider } from '@mui/material';
+import { createTheme, ThemeProvider, useMediaQuery } from '@mui/material';
 import { act, fireEvent, render, screen } from '@testing-library/react';
 
 import BaseLayout from './BaseLayout';
 
+jest.mock('@mui/material', () => {
+  const actual = jest.requireActual('@mui/material');
+
+  return {
+    ...actual,
+    useMediaQuery: jest.fn()
+  };
+});
+
 describe('<BaseLayout />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (useMediaQuery as jest.Mock).mockImplementation(() => false);
+  });
+
   it('should render properly', () => {
     const { container } = render(
       <ThemeProvider theme={createTheme()}>
@@ -29,6 +44,18 @@ describe('<BaseLayout />', () => {
       fireEvent.click(screen.getByRole('button'));
     });
 
-    expect(screen.getByRole('menubar')).toHaveStyle('padding-top: 0px')
+    expect(screen.getByRole('menubar')).toHaveStyle('padding-top: 0px');
+  });
+
+  it('should render the topBar', async () => {
+    (useMediaQuery as jest.Mock).mockClear().mockImplementation(() => true);
+
+    render(
+      <ThemeProvider theme={createTheme()}>
+        <BaseLayout header="Test header" selected="dashboard">
+          <div>Child</div>
+        </BaseLayout>
+      </ThemeProvider>
+    );
   });
 });
