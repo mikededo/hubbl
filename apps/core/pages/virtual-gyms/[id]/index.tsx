d202b404c0859279039d20e4976cdde6aaad75b0
Miquel de Domingo <miquelddg@gmail.com>
2022-03-26 16:17:55 +0100

    feat(core): added virtual gym page HBL-179 #time 2h 30m

--- apps/core/pages/virtual-gyms/[id]/index.tsx
+++ apps/core/pages/virtual-gyms/[id]/index.tsx
@@ -1,16 +1,61 @@
-import { PageHeader } from '@hubbl/ui/components';
+import { ReactElement, useEffect, useMemo } from 'react';
+
 import { useRouter } from 'next/router';
-import { ReactElement } from 'react';
+import useSWR from 'swr';
+
+import {
+  useAppContext,
+  useLoadingContext,
+  useToastContext
+} from '@hubbl/data-access/contexts';
+import { VirtualGymDTO } from '@hubbl/shared/models/dto';
+import { GymZoneGrid, PageHeader } from '@hubbl/ui/components';
 
 import { BaseLayout, GeneralPages } from '../../../components';
 
 const VirtualGym = () => {
   const router = useRouter();
-  
+  const { loading, onPopLoading, onPushLoading } = useLoadingContext();
+  const { onError } = useToastContext();
+  const {
+    token,
+    API: { fetcher }
+  } = useAppContext();
+  const { data, error } = useSWR<VirtualGymDTO>(
+    token.parsed ? `/virtual-gyms/${router.query.id}` : null,
+    fetcher
+  );
 
-  console.log(router.query.id)
+  const classGymZones = useMemo(() => {
+    if (!data) {
+      return [];
+    }
 
-    return <>
+    return data.gymZones.filter(({ isClassType }) => isClassType);
+  }, [data]);
+
+  const nonClassGymZones = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+
+    return data.gymZones.filter(({ isClassType }) => !isClassType);
+  }, [data]);
+
+  useEffect(() => {
+    if (!data && !loading) {
+      onPushLoading();
+    } else if (data && loading) {
+      onPopLoading();
+    }
+  }, [data, loading, onPopLoading, onPushLoading]);
+
+  if (error) {
+    onError(`${error}`);
+  }
+
+  return (
+    <>
       <PageHeader
         title="Virtual gym"
         breadcrumbs={[
@@ -18,7 +63,16 @@ const VirtualGym = () => {
           { href: '/virtual-gyms/id', label: 'Virtual gym name' }
         ]}
       />
-    </>;
+
+      <GymZoneGrid
+        header="Class gym zones"
+        href={`/virtual-gyms/${router.query.id}/gym-zones`}
+        gymZones={classGymZones}
+      />
+
+      <GymZoneGrid header="Non-class gym zones" gymZones={nonClassGymZones} />
+    </>
+  );
 };
 
 VirtualGym.getLayout = (page: ReactElement) => (
