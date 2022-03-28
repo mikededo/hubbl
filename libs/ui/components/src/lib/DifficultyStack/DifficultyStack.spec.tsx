e2a0b6e25664aecaa033cd96caf9c1731d8e62c7
Miquel de Domingo <miquelddg@gmail.com>
2022-03-28 17:16:33 +0200

    test(ui): test improvements HBL-189

--- libs/ui/components/src/lib/DifficultyStack/DifficultyStack.spec.tsx
+++ libs/ui/components/src/lib/DifficultyStack/DifficultyStack.spec.tsx
@@ -1,14 +1,32 @@
-import { render } from '@testing-library/react';
+import {screen,  render } from '@testing-library/react';
 
 import DifficultyStack from './DifficultyStack';
 
+const numberNames = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five'
+];
+
 describe('<DifficultyStack />', () => {
   it.each([1, 2, 3, 4, 5])(
-    'should render properly for difficulty $difficulty',
+    'should render properly for difficulty %d',
     (difficulty) => {
       const { container } = render(<DifficultyStack difficulty={difficulty} />);
 
       expect(container).toBeInTheDocument();
+      expect(screen.getByTitle('difficulty-one-active')).toBeInTheDocument();
+
+      for (let i = 2; i <= 5; i++) {
+        if (difficulty < i) {
+          expect(screen.getByTitle(`difficulty-${numberNames[i - 1]}-inactive`))
+          
+        } else {
+          expect(screen.getByTitle(`difficulty-${numberNames[i - 1]}-active`));
+        }
+      }
     }
   );
 });
--- libs/ui/components/src/lib/DifficultyStack/DifficultyStack.tsx
+++ libs/ui/components/src/lib/DifficultyStack/DifficultyStack.tsx
@@ -11,25 +11,35 @@ export type DifficultyStackProps = {
 
 const DifficultyStack = ({ difficulty }: DifficultyStackProps): JSX.Element => (
   <Stack direction="row">
-    <Fire key="difficulty-one" titleAccess="difficulty-one" color="primary" />
+    <Fire key="difficulty-one" titleAccess="difficulty-one-active" color="primary" />
     <Fire
       key="difficulty-two"
-      titleAccess="difficulty-two"
+      titleAccess={
+        difficulty >= 2 ? 'difficulty-two-active' : 'difficulty-two-inactive'
+      }
       color={difficulty >= 2 ? 'primary' : 'disabled'}
     />
     <Fire
       key="difficulty-three"
-      titleAccess="difficulty-three"
+      titleAccess={
+        difficulty >= 3
+          ? 'difficulty-three-active'
+          : 'difficulty-three-inactive'
+      }
       color={difficulty >= 3 ? 'primary' : 'disabled'}
     />
     <Fire
       key="difficulty-four"
-      titleAccess="difficulty-four"
+      titleAccess={
+        difficulty >= 4 ? 'difficulty-four-active' : 'difficulty-four-inactive'
+      }
       color={difficulty >= 4 ? 'primary' : 'disabled'}
     />
     <Fire
       key="difficulty-five"
-      titleAccess="difficulty-five"
+      titleAccess={
+        difficulty >= 5 ? 'difficulty-five-active' : 'difficulty-five-inactive'
+      }
       color={difficulty >= 5 ? 'primary' : 'disabled'}
     />
   </Stack>
