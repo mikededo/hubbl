09a3b6e4bdddb5825b5bc6b7a2915a73479dcc52
Miquel de Domingo <miquelddg@gmail.com>
2022-04-25 16:56:22 +0200

    chore(client): minor test changes HBL-229

--- apps/client/jest.config.ts
+++ apps/client/jest.config.ts
@@ -1,11 +1,13 @@
 module.exports = {
   displayName: 'client',
-
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }]
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/client',
+  collectCoverageFrom: ['./**/*.ts', './**/*.tsx', '!src/**/index.ts'],
+  coverageReporters: ['text', 'html'],
   preset: '../../jest.preset.ts'
 };
