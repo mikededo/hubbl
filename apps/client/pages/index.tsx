10000ff6c97f572f4a9c5150220c323fadb82bab
Miquel de Domingo <miquelddg@gmail.com>
2022-04-25 16:29:08 +0200

    feat(client): set up client structure
    HBL-150 #time 15m

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,3 +1,21 @@
-const Index = () => <div />;
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/router';
+
+import { useAppContext } from '@hubbl/data-access/contexts';
+
+const Index = () => {
+  const router = useRouter();
+  const { user } = useAppContext();
+
+  useEffect(() => {
+    if (!user) {
+      /* Redirect to login */
+      router.push('/auth/login');
+    }
+  }, [user, router]);
+
+  return <></>;
+};
 
 export default Index;
