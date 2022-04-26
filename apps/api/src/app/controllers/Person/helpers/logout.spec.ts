d2d22b7c52e9f3e34581ed9497b797ec23db0c6b
Miquel de Domingo <miquelddg@gmail.com>
2022-04-26 17:28:58 +0200

    feat(api): added logout routes + expiry time HBL-252 #time 10m

--- apps/api/src/app/controllers/Person/helpers/logout.spec.ts
+++ apps/api/src/app/controllers/Person/helpers/logout.spec.ts
@@ -1,25 +1,27 @@
-import { logout} from './logout';
+import { logout } from './logout';
 
 describe('logout', () => {
   const mockRes = {
     clearCookie: jest.fn()
-  }
+  };
 
   const mockController = {
     ok: jest.fn()
-  }
+  };
 
   it('should clear the cookie and return ok', async () => {
-    await logout({ res: mockRes, controller: mockController} as any);
+    jest.useFakeTimers().setSystemTime(new Date('2022/01/03'));
+    await logout({ res: mockRes, controller: mockController } as any);
 
     expect(mockRes.clearCookie).toHaveBeenCalledTimes(1);
     expect(mockRes.clearCookie).toHaveBeenCalledWith('__hubbl-refresh__', {
       sameSite: 'none',
       secure: true,
       httpOnly: true,
-      path: '/'
+      path: '/',
+      expires: new Date('2022/01/02')
     });
     expect(mockController.ok).toHaveBeenCalledTimes(1);
     expect(mockController.ok).toHaveBeenCalledWith(mockRes);
-  })
-})
+  });
+});
--- apps/api/src/app/controllers/Person/helpers/logout.ts
+++ apps/api/src/app/controllers/Person/helpers/logout.ts
@@ -11,11 +11,15 @@ export const logout = async ({
   controller,
   res
 }: LogoutProps): Promise<any> => {
+  const today = new Date();
+  today.setDate(today.getDate() - 1);
+
   res.clearCookie('__hubbl-refresh__', {
     sameSite: 'none',
     secure: true,
     httpOnly: true,
-    path: '/'
+    path: '/',
+    expires: today
   });
 
   return controller.ok(res);
