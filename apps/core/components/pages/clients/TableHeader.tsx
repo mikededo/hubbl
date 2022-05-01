148066d49f3d8368d75c6464da197fab5525b3d5
Miquel de Domingo <miquelddg@gmail.com>
2022-05-01 19:44:00 +0200

    fix(core): table header ui improvements HBL-203

--- apps/core/components/pages/clients/TableHeader.tsx
+++ apps/core/components/pages/clients/TableHeader.tsx
@@ -3,12 +3,12 @@ import { TableRow } from '@mui/material';
 
 const TableHeader = () => (
   <TableRow hover={false} sx={{ cursor: 'default' }}>
-    <HeaderCell width="24.5%">First name</HeaderCell>
-    <HeaderCell width="24.5%">Last name</HeaderCell>
-    <HeaderCell width="24.5%">Email</HeaderCell>
-    <HeaderCell width="24.5%">Phone</HeaderCell>
-    <HeaderCell width="1%">Covid passport</HeaderCell>
-    <HeaderCell width="1%">Gender</HeaderCell>
+    <HeaderCell width="21.5%">First name</HeaderCell>
+    <HeaderCell width="21.5%">Last name</HeaderCell>
+    <HeaderCell width="21.5%">Email</HeaderCell>
+    <HeaderCell width="21.5%">Phone</HeaderCell>
+    <HeaderCell width="12%">Covid passport</HeaderCell>
+    <HeaderCell width="2%">Gender</HeaderCell>
   </TableRow>
 );
 
