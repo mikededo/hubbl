6020a004fb61f90aff7ad71eb1f48bfc36a8472c
Miquel de Domingo <miquelddg@gmail.com>
2022-05-03 10:37:16 +0200

    feat(core): added animations and callbacks HBL-199 #time 25m

--- apps/core/components/pages/workers/WorkerPermissionsViewer.spec.tsx
+++ apps/core/components/pages/workers/WorkerPermissionsViewer.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+
 import WorkerPermissionsViewer from './WorkerPermissionsViewer';
 
 describe('<WorkerPermissionsViewer />', () => {
@@ -12,15 +13,24 @@ describe('<WorkerPermissionsViewer />', () => {
   });
 
   it('should render with a worker', () => {
+    const onEditSpy = jest.fn();
+    const onUnselectSpy = jest.fn();
+
     const utils = render(
       <WorkerPermissionsViewer
         worker={{ firstName: 'Worker', lastName: 'Name' } as any}
+        onEditClick={onEditSpy}
+        onUnselectClick={onUnselectSpy}
       />
     );
+    fireEvent.click(utils.getByLabelText('edit-worker'));
+    fireEvent.click(utils.getByLabelText('unselect-worker'));
 
     expect(utils.getByText('Worker Name')).toBeInTheDocument();
     expect(
       utils.queryByText('Click a worker to see their permissions')
     ).not.toBeInTheDocument();
+    expect(onEditSpy).toHaveBeenCalledTimes(1);
+    expect(onUnselectSpy).toHaveBeenCalledTimes(1);
   });
 });
