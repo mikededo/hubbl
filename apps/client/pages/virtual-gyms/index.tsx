534a6914723c52beb931e3563e9ffe5227f1b1df
Miquel de Domingo <miquelddg@gmail.com>
2022-05-09 14:44:19 +0200

    feat(client): display list of virtual gyms HBL-242 #time 15m

--- apps/client/pages/virtual-gyms/index.tsx
+++ apps/client/pages/virtual-gyms/index.tsx
@@ -1,17 +1,40 @@
 import { ReactElement } from 'react';
 
-import { PageHeader } from '@hubbl/ui/components';
+import useSWR from 'swr';
+
+import { useAppContext, useToastContext } from '@hubbl/data-access/contexts';
+import { VirtualGymDTO } from '@hubbl/shared/models/dto';
+import { PageHeader, VirtualGymList } from '@hubbl/ui/components';
 
 import { BaseLayout, GeneralPages } from '../../components';
 
-const VirtualGyms = () => (
-  <>
-    <PageHeader
-      title="Virtual gyms"
-      breadcrumbs={[{ href: '/', label: 'Virtual Gyms' }]}
-    />
-  </>
-);
+const VirtualGyms = () => {
+  const { onError } = useToastContext();
+  const {
+    token,
+    API: { fetcher }
+  } = useAppContext();
+  const { data, error } = useSWR<VirtualGymDTO[]>(
+    token.parsed ? '/virtual-gyms' : null,
+    fetcher,
+    { revalidateOnFocus: false }
+  );
+
+  if (error) {
+    onError(error);
+  }
+
+  return (
+    <>
+      <PageHeader
+        title="Virtual gyms"
+        breadcrumbs={[{ href: '/', label: 'Virtual Gyms' }]}
+      />
+
+      {data && <VirtualGymList virtualGyms={data} />}
+    </>
+  );
+};
 
 export default VirtualGyms;
 
